3b342e1b9a81ac29cc3e04308b31dfd3a12b856f
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-04-04 21:12:36 +0600

    Some ui on ember

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,56 @@
-App = Ember.Application.create();
+/* global App, Ember, moment */
+(function () {
 
-App.Router.map(function() {
-  // put your routes here
-});
+    App = Ember.Application.create();
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
+    App.Router.map(function () {
+        this.resource('join', { path: '/' });
+        this.resource('room', function () {
+            this.route('view', { path: '/:room_name' });
+        });
+    });
+
+    App.RoomRoute = Ember.Route.extend({
+        model: function () {
+            return {
+                messages: [{
+                    author: 'Name',
+                    content: 'Hello!',
+                    date: Date()
+                },{
+                    author: 'Anonymous',
+                    content: 'Hi!',
+                    date: Date()
+                }],
+                users: ['Name', 'Anonymous']
+            };
+        }
+    });
+
+    App.ActiveRoomsView = Ember.View.create({
+        templateName: 'active-rooms',
+        rooms: Ember.ArrayController.create()
+    });
+
+    App.ActiveRoomsView.rooms.set('content', ['Swarm']);
+
+    App.JoinRoute = Ember.Route.extend({
+        actions: {
+            goToLink: function(item) {
+                App.ActiveRoomsView.rooms.addObject(item);
+                this.transitionTo('room.view', item);
+            }
+        }
+    });
+
+    Ember.Handlebars.helper('format-date', function (value, options) {
+        return moment(value).calendar();
+    });
+
+// var peer = new Peer({
+//     host: '/',
+//     port: window.location.hostname === 'localhost' ? 5000 : 80,
+//     path: '/api/'
+// });
+
+})();
